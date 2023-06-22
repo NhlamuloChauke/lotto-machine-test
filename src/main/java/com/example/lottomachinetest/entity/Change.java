aeb5e650814ede9b3c2270d726fae90373a5631d
Nhlamulo Socha <nchauke@csir.co.za>
2023-06-22 09:14:51 +0200

    added change pojo

--- src/main/java/com/example/lottomachinetest/entity/Change.java
+++ src/main/java/com/example/lottomachinetest/entity/Change.java
@@ -1,4 +1,22 @@
 package com.example.lottomachinetest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
 public class Change {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String denomination;
+    @Column
+    private int quantity;
 }
